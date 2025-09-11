e9be35d9a8db2c4849e36dd19874c7658d7e1f49
Ms-Schnitzel <zachnielsen72@gmail.com>
2025-09-11 08:44:24 -0600

    BOM JS add

---
+++ Week02/scripts/bom.js
@@ -0,0 +1,34 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+
+// li.textContent = input.value;
+// deleteButton.textContent = '❌';
+
+// li.append(deleteButton);
+// list.append(li);
+
+button.addEventListener("click", function () {
+  if (input.value.trim() !== "") {
+    li.textContent = input.value;
+    deleteButton.textContent = '❌';
+
+    li.append(deleteButton);
+    list.append(li);
+
+    deleteButton.addEventListenter("click", function () {
+      list.removeChild(li);
+      input.focus();
+    })
+
+    input.value = "";
+    input.focus();
+  } else {
+    alert("Please enter your Book and Chapter!")
+    input.focus();
+  }
+})
+
